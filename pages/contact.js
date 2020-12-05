498a84d288eed7897e81c2a138d17a85b4d1e3a9
Josh Lavely <JLavely@smartronix.com>
2020-12-04 21:03:30 -0500

    Updated Pages and theme to use Responsive font sizes

---
+++ pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from '../theme'
+
+export default Contact = () => (
+  <>
+    <Head>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+      />
+      <link rel='manifest' href='site.webmanifest.json'></link>
+      <title>Wash Houston - Contact Us</title>
+    </Head>
+    <ThemeProvider theme={theme}></ThemeProvider>
+  </>
+)
