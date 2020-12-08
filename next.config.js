52e9189d6b4a0590f276b6dab5e3e070cb6a1ac8
Josh Lavely <JLavely@smartronix.com>
2020-12-08 12:36:01 -0500

    config updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
-const withOptimizedImages = require('next-optimized-images')
-
-module.exports = {
+const withImages = require('next-images')
+module.exports = withImages({
   env: {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     SENDGRID_TO: process.env.SENDGRID_SEND_TO,
     SENDGRID_SENDER_EMAIL: process.env.SENDGRID_SENDER_EMAIL,
   },
-}
+})
