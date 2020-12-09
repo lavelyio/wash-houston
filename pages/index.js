2eb294b47c8dd77648c582539ae34b309abfec94
Josh Lavely <JLavely@smartronix.com>
2020-12-08 23:26:02 -0500

    Setup analytics

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ const Home = () => (
   <>
     <Head>
       <title>Wash Houston</title>
+      <script async src='https://www.googletagmanager.com/gtag/js?id=G-431QJNV24K'></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-431QJNV24K');
+      </script>
     </Head>
     <Initial />
   </>
