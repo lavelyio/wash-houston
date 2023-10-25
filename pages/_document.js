f799e13a706bcde801a2859db5c273a5b693ef70
Josh Lavely <lavejos@chrobinson.com>
2023-10-25 19:44:23 -0400

    updated doc

--- pages/_document.js
+++ pages/_document.js
@@ -73,7 +73,7 @@ MyDocument.getInitialProps = async (ctx) => {
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     })
 
-  const initialProps = await Document.getInitialProps(ctx)
+  const initialProps = await Document.getInitialProps(ctx);
 
   return {
     ...initialProps,
